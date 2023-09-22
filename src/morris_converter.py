84d01ce5209dfaaa5cc507c87b0acc81a15bd136
T. Amburgey <timothy.amburgey@cognitronix-llc.com>
2023-09-22 14:16:33 -0400

    create converter class

---
+++ src/morris_converter.py
@@ -0,0 +1,17 @@
+class MorrisCodeConverter:
+
+    @staticmethod
+    def convert_text(text: str):
+        morse_code_dict = {
+            'A': '.-', 'B': '-...', 'C': '-.-.', 'D': '-..', 'E': '.',
+            'F': '..-.', 'G': '--.', 'H': '....', 'I': '..', 'J': '.---',
+            'K': '-.-', 'L': '.-..', 'M': '--', 'N': '-.', 'O': '---',
+            'P': '.--.', 'Q': '--.-', 'R': '.-.', 'S': '...', 'T': '-',
+            'U': '..-', 'V': '...-', 'W': '.--', 'X': '-..-', 'Y': '-.--',
+            'Z': '--..'
+        }
+        morris_text = ""
+        for letter in list(text):
+            morris_text = morris_text + morse_code_dict[letter.upper()]
+        return morris_text
+    
